8a523ed77c6eabf3c80c03414bdb98a59dfe0c4f
xuhao <1158238984@qq.com>
2018-03-28 14:05:03 +0800

    a project named hello_git

---
+++ src/main/java/com/xuhao/git/hello_git/App.java
@@ -0,0 +1,13 @@
+package com.xuhao.git.hello_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
